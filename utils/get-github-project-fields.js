a91f2de34442ebd6c02b01670d2c7c7b3f6cfd83
Grace Zongo <zongraceo@gmail.com>
2024-06-10 15:27:42 -0500

    update

--- utils/get-github-project-fields.js
+++ utils/get-github-project-fields.js
@@ -3,7 +3,9 @@
 let gh = require("../lib/gh.js");
 
 async function main() {
-  let fields = await gh.projects.getCustomFields();
+  let fields = await gh.projects.getCustomFields({
+    name: process.env.GITHUB_PROJECT_NAME // Ensure this is set in your environment variables
+  });
   fields.forEach(function (field) {
     console.info(`"${field.id}": "${field.name}"`);
   });
